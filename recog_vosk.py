592acbb21a81168a1127a2c181b5bb675bc53a08
chen <74883541+darKred9@users.noreply.github.com>
2024-05-01 18:25:59 -0400

    Update recog_vosk.py

--- recog_vosk.py
+++ recog_vosk.py
@@ -19,7 +19,7 @@ pygame.init()
 
 window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Smart Home Gesture")
+pygame.display.set_caption("Simulation")
 
 lights = [
     {"pos": (200, 200), "on": False, "timer": 0},
